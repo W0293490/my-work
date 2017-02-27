8c7cfe4d874fe111927bf228733ebe3db1f1cfa1
w0293490 <wo293490@campus.nscc.ca>
2017-02-27 00:26:33 -0400

    First commit of Special Topics lab 4 pt 1...

---
+++ SeanStringCalculatorTestProject/SeanStringCalculatorTestProject/SeanStringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeanStringCalculatorTestProject
+{
+    public class SeanStringCalculator
+    {
+        public static int add(string numbers)
+        {
+            int returnValue = 0;
+            string[] numbersArray = numbers.Split(',', '\n');
+            //if (numbersArray.Length > 2)
+            //{
+            //    throw new ArgumentOutOfRangeException("nope");
+            //}
+            
+            foreach (String number in numbersArray)
+            {
+                if (number.Trim() != null)
+                { // After refactoring
+                    returnValue += Int32.Parse(number);
+                }
+            }
+            //if (numbers.Length == 0)
+            //{
+            //    returnValue = 0;
+            //}
+            return returnValue;
+
+        }
+
+        public static object add()
+        {
+            return 0;
+        }
+
+        //public static int add()
+        //{
+        //    return 0;
+        //}
+    }
+}
---
+++ SeanStringCalculatorTestProject/StringCalculatorTest/SeanStringCalculatorTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeanStringCalculatorTestProject
+{
+    [TestClass]
+    public class SeanStringCalculatorTest
+    {
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
+
+        //public void whenMoreThan2NumbersAreUsedThenExceptionIsThrown()
+        //{
+        //    SeanStringCalculator.add("1,2,3");
+        //}
+
+        [TestMethod]
+        public void when2NumbersAreUsedThenNoExceptionIsThrown()
+        {
+            SeanStringCalculator.add("1,2");
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void whenEmptyStringIsUsedThenReturnValueIs0()
+        {
+            Assert.AreEqual(0, SeanStringCalculator.add());
+                ;
+        }
+
+        [TestMethod]
+        public void whenOneNumberIsUsedThenReturnValueIsThatSameNumber()
+        {
+            Assert.AreEqual(3, SeanStringCalculator.add("3"));
+        }
+
+        [TestMethod]
+        public void whenTwoNumbersAreUsedThenReturnValueIsTheirSum()
+        {
+            Assert.AreEqual(3 + 6, SeanStringCalculator.add("3,6"));
+        }
+
+        [TestMethod]
+        public void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums()
+        {
+            Assert.AreEqual(3 + 6 + 15 + 18 + 46 + 33, SeanStringCalculator.add("3,6,15,18,46,33"));
+        }
+
+        [TestMethod]
+        public void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums()
+        {
+            Assert.AreEqual(3 + 6 + 15, SeanStringCalculator.add("3,6\n15"));
+        }
+
+
+    }
+}
+
+
+
+//public class StringCalculatorTest
+//{
+//    @Test(expected = RuntimeException.class)
+//    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown()
+//    {
+//        StringCalculator.add("1,2,3");
+//    }
+//    @Test
+//    public final void when2NumbersAreUsedThenNoExceptionIsThrown()
+//    {
+//        StringCalculator.add("1,2");
+//        Assert.assertTrue(true);
+//    }
+//    @Test(expected = RuntimeException.class)
+//    public final void whenNonNumberIsUsedThenExceptionIsThrown()
+//    {
+//        StringCalculator.add("1,X");
+//    }
+//}
+
